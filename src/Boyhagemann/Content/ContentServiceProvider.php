a4a7cebec5ad31e067078bbedf47b6ef4d302206
= <=>
2013-10-21 14:29:30 +0200

    added content models

--- src/Boyhagemann/Content/ContentServiceProvider.php
+++ src/Boyhagemann/Content/ContentServiceProvider.php
@@ -1,7 +1,9 @@
 <?php namespace Boyhagemann\Content;
 
 use Illuminate\Support\ServiceProvider;
-use Boyhagemann\Content\Model\Resource;
+use Boyhagemann\Content\Model\Block;
+use Boyhagemann\Pages\Model\Section;
+use Boyhagemann\Pages\Model\Page;
 use Route, Form, App, Event;
 
 class ContentServiceProvider extends ServiceProvider {
@@ -22,6 +24,26 @@ class ContentServiceProvider extends ServiceProvider {
 	{
 		$this->package('boyhagemann/content');
 
+
+		Event::listen('page.createWithContent', function(Page $page) {
+
+			$block = Block::whereController($page->controller)->first();
+			$section = Section::whereName('content')->first();
+
+			$content = new Model\Content;
+			$content->page()->associate($page);
+			$content->section()->associate($section);
+
+			if($block) {
+				$content->block()->associate($block);
+			}
+			else {
+				$content->controller = $page->controller;
+			}
+
+			$content->save();
+
+		});
 	}
 
 	/**
