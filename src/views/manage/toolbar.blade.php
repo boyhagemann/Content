88efdd2f8cc4cbcbad733566bb815b2000da273e
= <=>
2013-10-24 14:16:01 +0200

    remove css toolbar

--- src/views/manage/toolbar.blade.php
+++ src/views/manage/toolbar.blade.php
@@ -1,11 +1,12 @@
-<section class="content_section">
-	<header>
 
-	</header>
-
-	@if($mode == 'view')
-	<a href="?mode=content">Switch to content mode</a>
-	@else
-	<a href="?mode=view">Switch to view mode</a>
-	@endif
-</section>
+<nav class="content_toolbar navbar navbar-default navbar-fixed-bottom" role="content">
+		<ul class="nav navbar-nav pull-right">
+			<li>
+				@if($mode == 'view')
+				<a href="?mode=content"><i class="icon-edit"></i> Switch to content mode</a>
+				@else
+				<a href="?mode=view"><i class="icon-eye-open"></i> Switch to view mode</a>
+				@endif
+			</li>
+		</ul>
+</nav>
