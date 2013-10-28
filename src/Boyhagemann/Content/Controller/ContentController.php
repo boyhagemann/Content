0315eb2922c1b381055c2df4fb334110d8d5a24b
= <=>
2013-10-28 17:12:10 +0100

    update comments

--- src/Boyhagemann/Content/Controller/ContentController.php
+++ src/Boyhagemann/Content/Controller/ContentController.php
@@ -27,15 +27,6 @@ class ContentController extends CrudController
 	}
 
 	/**
-	 * @param Content $content
-	 */
-	public function onSaved(Content $content)
-	{
-		$redirect = Redirect::route('admin.content.config.edit', $content->id);
-		return ResponseException::chain($redirect)->fire();
-	}
-
-	/**
 	 * @param ModelBuilder $mb
 	 */
 	public function buildModel(ModelBuilder $mb)
@@ -48,6 +39,26 @@ class ContentController extends CrudController
 	 */
 	public function buildOverview(OverviewBuilder $ob)
 	{
+	}
+
+	/**
+	 * Redirect to the content configuration form.
+	 *
+	 * Right after the content block has been created, we can hook into the
+	 * save method and redirect to a configuration form. There we can set
+	 * the needed params required for this content block to work.
+	 *
+	 * It uses the ResponseException instead of the normal Redirect object.
+	 * This is because a normal redirect will not work, because that
+	 * object is never returned as a view. But now we throw a special
+	 * exception that will do the trick for us.
+	 *
+	 * @param Content $content
+	 */
+	public function onSaved(Content $content)
+	{
+		$redirect = Redirect::route('admin.content.config.edit', $content->id);
+		return ResponseException::chain($redirect)->fire();
 	}
 
 }
